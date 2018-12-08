fcc3abf9f3f5be44fd61988c04b7d3d0263c331c
Camel <camelchen@qq.com>
2018-12-07 22:12:31 -0500

    add new type of logger: ConsoleLogger

---
+++ Selenium.Framework.Core/Logger/ConsoleLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Selenium.Framework.Core.Logger
+{
+    public static class ConsoleLogger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(message);
+        }
+    }
+}
--- Selenium.Framework.Core/Site/WebPage.cs
+++ Selenium.Framework.Core/Site/WebPage.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using Selenium.Framework.Core.DOMElement;
 using Selenium.Framework.Core.Driver;
+using Selenium.Framework.Core.Logger;
 using System;
 using System.Collections.Generic;
 
@@ -26,6 +27,7 @@ namespace Selenium.Framework.Core
             {
                 this.driver = (ChromeDriver)Browser.Open(Browser.Type.Chrome);
                 driver.Navigate().GoToUrl(url);
+                ConsoleLogger.Log("Open url: " + url);
             }
         }
 
@@ -34,11 +36,13 @@ namespace Selenium.Framework.Core
         {
             driver.FindElement(By.Id(id)).Clear();
             driver.FindElement(By.Id(id)).SendKeys(text);
+            ConsoleLogger.Log("Edit InputText: " + text);
         }
 
         public void Click(string id)
         {
             driver.FindElement(By.Id(id)).Click();
+            ConsoleLogger.Log("Click: " + driver.FindElement(By.Id(id)).Text);
         }
 
         public void Clicks(string id)
@@ -48,6 +52,7 @@ namespace Selenium.Framework.Core
             for (int i = 1; i <= nav_items_count; i++)
             {
                 driver.FindElement(By.Id(id)).FindElement(By.CssSelector("li:nth-child("+i.ToString()+") > a")).Click();
+                ConsoleLogger.Log("Click: " + driver.FindElement(By.Id(id)).FindElement(By.CssSelector("li:nth-child(" + i.ToString() + ")")).Text);
                 WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
             }
             
